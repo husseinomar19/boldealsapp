bab21771faac343cfceb1f190b7df31b3bc70d97
Hussein <omerhussen603@gmail.com>
2025-04-03 14:46:50 +0200

    Set up NativeWind and remove unused components and hooks

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,10 @@
+
+import { Text, SafeAreaView } from "react-native";
+
+export default function Search() {
+  return (
+    <SafeAreaView>
+        <Text className="text-black ">Search</Text>
+    </SafeAreaView>
+  );
+}
